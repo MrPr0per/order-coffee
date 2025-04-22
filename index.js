40a404c7ebef27ef61dbb26af0520f1ef6d19c87
g_host <@>
2025-04-22 17:16:55 +0500

    фикс мержа

--- index.js
+++ index.js
@@ -5,18 +5,6 @@ updateForms();
 addEventListeners()
 
 function addEventListeners() {
-    const overlay = document.querySelector('.overlay');
-    const closeButton = document.getElementById('close');
-    closeButton.addEventListener('click', () => {
-        overlay.style.display = 'none';
-    });
-
-    const submitButton = document.querySelector('.submit-button');
-    submitButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        overlay.style.display = 'flex';
-    });
-
     addEventListenerOnTextarea(document);
 
     const popupContent = document.querySelector('.popup__content');
