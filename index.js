3d43868b49932d43268ee1561bebba3cce6ad184
g_host <@>
2025-04-22 16:31:15 +0500

    2 (удаление форм)

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const addBeverageButton = document.getElementById('add-drink-button');
-document.querySelector('.beverage').outerHTML;
 addBeverageButton.addEventListener('click', () => addNewBeverage());
+
+updateForms();
+
 
 function addNewBeverage() {
     const beverages = document.querySelectorAll('.beverage');
     const newNumber = beverages.length + 1;
-
     const newBeverage = beverages[0].cloneNode(true);
-
-    newBeverage.querySelector('.beverage-count').textContent = `Напиток №${newNumber}`;
 
     newBeverage.querySelectorAll('input[type="radio"]').forEach((radio) =>
         radio.name = `milk-${newNumber}`
     );
-
     newBeverage.querySelectorAll('input[type="checkbox"]').forEach((checkbox) =>
         checkbox.name = `options-${newNumber}`
     );
 
     const form = document.querySelector('form');
     form.insertBefore(newBeverage, addBeverageButton.parentElement);
+
+    updateForms();
 }
 
 const overlay = document.querySelector('.overlay');
@@ -33,3 +33,31 @@ submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     overlay.style.display = 'flex';
 });
+
+function updateForms() {
+    updateNumbers();
+    updateRemoveButtons();
+}
+
+function updateNumbers() {
+    const beverages = document.querySelectorAll(".beverage");
+    beverages.forEach((beverage, index) => {
+        beverage.querySelector(".beverage-count").textContent = `Напиток №${index + 1}`;
+    });
+}
+
+function updateRemoveButtons() {
+    const removeButtons = document.querySelectorAll(".remove-button");
+    const beverages = document.querySelectorAll(".beverage");
+
+    const shouldHide = beverages.length === 1;
+
+    removeButtons.forEach((btn) => {
+        btn.style.display = shouldHide ? "none" : "block";
+
+        btn.onclick = () => {
+            btn.parentElement.remove();
+            updateForms();
+        };
+    });
+}
